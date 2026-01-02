dfc9436a63cd370c4f9cbc04cb56348d3bcfabd3
Vadim Tokaev <fw.mbagg@gmail.com>
2026-01-02 18:52:38 +0300

    add meeting item

---
+++ src/components/MeetingItem/MeetingItem.tsx
@@ -0,0 +1,33 @@
+import "./MeetingItem.css";
+import type { MeetingType } from "../../types/MeetingType";
+
+type MeetingItemProps = Omit<MeetingType, "location" | "id">;
+
+const formatTime = (date: Date) => {
+    return `${date.getHours()}:${date.getMinutes()}`;
+};
+
+function MeetingItem({ person, date, links }: MeetingItemProps) {
+    const { telegram } = links;
+
+    return (
+        <div className="meeting_row">
+            <div className="meeting_row__info">
+                <div className="meeting_row__bullet" />
+                <span className="meeting_row__person">{person}</span>
+                <a
+                    className="meeting_row__telegram"
+                    href={telegram}
+                    target="_blank"
+                >
+                    <img src="./icons/telegram.svg" />
+                </a>
+            </div>
+            <div className="meeting_row__date">
+                <span className="meeting_row__time">{formatTime(date)}</span>
+            </div>
+        </div>
+    );
+}
+
+export default MeetingItem;
---
+++ src/types/MeetingType.ts
@@ -0,0 +1,16 @@
+export type MeetingType = {
+    id: number;
+    /** ФИО заказчика **/
+    person: string;
+    /** Место встречи **/
+    location?: string;
+    /** Дата встречи **/
+    date: Date;
+    /** Ссылки **/
+    links: {
+        /** Telegram заказчика **/
+        telegram: string;
+        /** Ссылка на портфолио с выполненной работой **/
+        wfolio?: string;
+    };
+};
