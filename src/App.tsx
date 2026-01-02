f95a75c088261fe70d3a66e6761dac3ba7285ef6
Vadim Tokaev <fw.mbagg@gmail.com>
2026-01-02 19:05:30 +0300

    add commont component Amount

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
 import "./App.css";
+
 import Block from "./components/common/Block/Block";
 import MeetingItem from "./components/MeetingItem/MeetingItem";
 
 function App() {
     return (
         <>
-            <Block title="Ближайшие встречи">
-                <MeetingItem
-                    person="Vadim T."
-                    date={new Date()}
-                    links={{ telegram: "https://t.me/neblessed" }}
-                />
-                <MeetingItem
-                    person="Julia K."
-                    date={new Date()}
-                    links={{ telegram: "https://t.me/neblessed" }}
-                />
-                <MeetingItem
-                    person="Arnold S."
-                    date={new Date()}
-                    links={{ telegram: "https://t.me/neblessed" }}
-                />
-                <MeetingItem
-                    person="Tom K."
-                    date={new Date()}
-                    links={{ telegram: "https://t.me/neblessed" }}
-                />
-                {/* <p>Ильзира Сильверстайн – 18:00</p>
-                <p>Вадим Токаев – 22:00</p>
-                <p>Ещё кто-то – 08:00</p>
-                <button>Continue</button> */}
-            </Block>
+            <div className="app_row1">
+                <Block title="Ближайшие встречи">
+                    <MeetingItem
+                        person="Vadim T."
+                        date={new Date()}
+                        links={{ telegram: "https://t.me/neblessed" }}
+                    />
+                    <MeetingItem
+                        person="Julia K."
+                        date={new Date()}
+                        links={{ telegram: "https://t.me/neblessed" }}
+                    />
+                    <MeetingItem
+                        person="Arnold S."
+                        date={new Date()}
+                        links={{ telegram: "https://t.me/neblessed" }}
+                    />
+                    <MeetingItem
+                        person="Tom K."
+                        date={new Date()}
+                        links={{ telegram: "https://t.me/neblessed" }}
+                    />
+                </Block>
+            </div>
         </>
     );
 }
