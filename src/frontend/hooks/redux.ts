93dc700538442296b04b9e21d2adf12f231b60e8
Vadim Tokaev <fw.mbagg@gmail.com>
2026-01-12 20:54:02 +0300

    separate frontend and backend

--- src/frontend/hooks/redux.ts
+++ src/frontend/hooks/redux.ts
@@ -3,7 +3,7 @@ import {
     useSelector,
     type TypedUseSelectorHook,
 } from "react-redux";
-import type { AppDispatch, RootState } from "../store";
+import type { AppDispatch, RootState } from "../../store";
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
